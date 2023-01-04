6ea0aa2b2d4b82ba074bae65b14e18223b7c0f52
Jasoc <paridegiunta@gmail.com>
2023-01-04 01:06:21 +0100

    Cleaned comments

--- scripts/build.sh
+++ scripts/build.sh
@@ -19,51 +19,26 @@ mkdir dist/debug/
 
 for proj in ${pythonProjects[@]}
 do
-    # found=false
-    # if [[ $1 == "debug" ]]; then
-    #     for d in $proj/*
-    #     do
-    #         if [[ "$d" == *.egg-info ]]; then
-    #             found=true
-    #         elif [[ "$d" == */src ]]; then
-    #             for dd in $d/*
-    #             do
-    #                 if [[ "$dd" == *.egg-info ]]; then
-    #                     found=true
-    #                 fi
-    #             done
-    #         fi
-    #     done
-    # else
-    #     if [ -d dist/prod/$proj ]; then
-    #         found=true
-    #     fi
-    # fi
-    # if $found && ! $forceBuild
-    # then
-    #     echo "$proj already installed."
-    # else
-        cd $clip_root/$proj
-        
-        if [[ $1 == "debug" ]]; then
-            pip install --editable .
-        fi
-        
-        if [[ $1 == "prod" || $# == 0 ]]; then
-            python -m build --outdir ../dist/prod/$proj
-            pip install ../dist/prod/$proj/*.whl
-        fi
+    cd $clip_root/$proj
+    
+    if [[ $1 == "debug" ]]; then
+        pip install --editable .
+    fi
+    
+    if [[ $1 == "prod" || $# == 0 ]]; then
+        python -m build --outdir ../dist/prod/$proj
+        pip install ../dist/prod/$proj/*.whl
+    fi
 
-        if [[ $1 == "build" ]]; then
-            python -m build --outdir ../dist/prod/$proj
-        fi
+    if [[ $1 == "build" ]]; then
+        python -m build --outdir ../dist/prod/$proj
+    fi
 
-        if [[ $1 == "install" ]]; then
-            pip install ../dist/prod/$proj/*.whl
-        fi
+    if [[ $1 == "install" ]]; then
+        pip install ../dist/prod/$proj/*.whl
+    fi
 
-        if [[ $1 == "install-docker" ]]; then
-            pip install *.whl
-        fi
-    # fi
+    if [[ $1 == "install-docker" ]]; then
+        pip install *.whl
+    fi
 done
