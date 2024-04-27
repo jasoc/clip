cc2610c13bff4cfc34d1d5e5606893fdf023a069
jasoc <paridegiunta@gmail.com>
2024-04-27 23:29:57 +0200

    New compose layout

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -8,5 +8,6 @@ docker image rm clip/spa:debug clip/apiserver:debug clip/spa clip/apiserver
 rm -r $CLIP_PROJECT_PATH/dist
 rm -r $CLIP_PROJECT_PATH/spa/dist
 rm -r $CLIP_PROJECT_PATH/spa/node_modules
+rm -r $CLIP_PROJECT_PATH/spa/.angular
 rm -r $CLIP_PROJECT_PATH/apiserver/.venv
 rm -r $CLIP_PROJECT_PATH/apiserver/dist
--- spa/src/app/modules/home/home.component.ts
+++ spa/src/app/modules/home/home.component.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
 import { M3ButtonComponent } from '../../components/m3-button/m3-button.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { M3IconComponent } from "../../components/m3-icon/m3-icon.component";
 
 @Component({
-  selector: 'clip-home',
-  standalone: true,
-  imports: [
-    M3ButtonComponent
-  ],
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+    selector: 'clip-home',
+    standalone: true,
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.scss'],
+    imports: [
+        M3ButtonComponent,
+        MatButtonModule,
+        MatIconModule,
+        M3IconComponent
+    ]
 })
 export class HomeMainComponent {
 
