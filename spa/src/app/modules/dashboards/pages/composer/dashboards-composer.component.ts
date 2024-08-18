bc1c51c2958ed5b5118dcb45fabf740502f8bfa7
Jasoc <paridegiunta@gmail.com>
2024-08-19 00:30:44 +0200

    We have a sider in the composer

--- spa/src/app/modules/dashboards/pages/composer/dashboards-composer.component.ts
+++ spa/src/app/modules/dashboards/pages/composer/dashboards-composer.component.ts
@@ -1,20 +1,55 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { GridstackComponent, gsCreateNgComponents, NgGridStackOptions, NgGridStackWidget, GridstackModule } from 'gridstack/dist/angular';
+import {
+  GridstackComponent,
+  gsCreateNgComponents,
+  NgGridStackOptions,
+  NgGridStackWidget,
+  GridstackModule,
+} from 'gridstack/dist/angular';
 import { MatButtonModule } from '@angular/material/button';
 import { DashboardsWidgetCardComponent } from '../../widgets/card/dashboards-widget-card.component';
 import { GridStack } from 'gridstack';
+import { M3TabsComponent } from '../../../../components/m3-tabs/m3-tabs.component';
+import { M3TabComponent } from '../../../../components/m3-tabs/m3-tab/m3-tab.component';
+import { MatIconModule } from '@angular/material/icon';
+import { M3IconComponent } from '../../../../components/m3-icon/m3-icon.component';
+import { animate, animateChild, group, query, state, style, transition, trigger } from '@angular/animations';
+import { simpleFade } from '../../../../animations/enterAndLeave';
 
 @Component({
   selector: 'clip-dashboards-composer',
   standalone: true,
   templateUrl: './dashboards-composer.component.html',
   styleUrls: ['./dashboards-composer.component.scss'],
-  imports: [GridstackModule, MatButtonModule],
+  imports: [GridstackModule, MatButtonModule, MatIconModule, M3TabsComponent, M3TabComponent, M3IconComponent],
+  animations: [
+    trigger('showSiderContent', simpleFade('200ms')),
+    trigger('collapseSider', [
+      state(
+        'true',
+        style({
+          width: "20px",
+          padding: "0px",
+        })
+      ),
+      state(
+        'false',
+        style({
+          width: "340px",
+          padding: "16px",
+        })
+      ),
+      transition('true <=> false', [
+        group([query('@*', animateChild()), animate('280ms cubic-bezier(0.18, 0.89, 0.32, 1)')]),
+      ]),
+    ]),
+  ],
 })
 export class DashboardsComposerComponent implements OnInit {
   @ViewChild(GridstackComponent) gridComp?: GridstackComponent;
 
   public ids: number = 0;
+  public siderCollapsed: boolean = false;
 
   // public children: NgGridStackWidget[] = [{selector:'clip-dashboards-widget-card'}, {selector:'clip-dashboards-widget-card'}, {selector:'clip-dashboards-widget-card'}, {selector:'clip-dashboards-widget-card'}];
 
@@ -22,14 +57,12 @@ export class DashboardsComposerComponent implements OnInit {
     margin: 5,
     // float: true,
     minRow: 1,
-    column: 11,
     cellHeight: 40,
     // columnOpts: { breakpoints: [{ w: 768, c: 1 }] },
     // children: this.children
   };
 
-  constructor(
-  ) {
+  constructor() {
     GridstackComponent.addComponentToSelectorType([DashboardsWidgetCardComponent]);
   }
 
@@ -39,6 +72,18 @@ export class DashboardsComposerComponent implements OnInit {
 
   public add() {
     if (!this.gridComp?.el) return;
-    this.gridComp?.grid?.addWidget({x: 1, y: 1, h: 3, w: 3, selector:'clip-dashboards-widget-card'} as NgGridStackWidget);
+    this.gridComp?.grid?.addWidget({
+      x: 1,
+      y: 1,
+      h: 3,
+      w: 3,
+      selector: 'clip-dashboards-widget-card',
+    } as NgGridStackWidget);
+    this.gridComp?.grid?.save();
+  }
+
+  toggleSiderCollapse() {
+    this.siderCollapsed = !this.siderCollapsed;
+    console.log(this.siderCollapsed);
   }
 }
