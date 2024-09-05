d32513189083dcd3d781ddbbfb37f88f9db65987
Jasoc <paridegiunta@gmail.com>
2024-09-06 00:01:43 +0200

    New service indtroduced: The agent. (only base project, very very useless)

---
+++ agent/Program.cs
@@ -0,0 +1,16 @@
+﻿using Docker.DotNet;
+
+var test = new Dictionary<string, dynamic>() { };
+
+Console.WriteLine("Hello, World!");
+
+DockerClient client = new DockerClientConfiguration(
+    new Uri("http://ubuntu-docker.cloudapp.net:4243"))
+     .CreateClient();
+
+// IList<ContainerListResponse> containers = await client.Containers.ListContainersAsync(
+//     new ContainersListParameters()
+//     {
+//         Limit = 10,
+//     }
+// );
---
+++ agent/agent.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+develop_agent()
+{
+    dotnet watch
+}
+
+# run_agent()
+# {
+
+# }
+
+# build_agent()
+# {
+
+# }
+
+
+# prettify()
+# {
+
+# }
+
+main()
+{
+    case $1 in
+        '')       develop_agent;;
+        dev)      develop_agent;;
+        # run)      run_agent;;
+        # build)    build_agent;;
+        prettify) prettify;;
+        *) $@
+    esac
+}
+
+main "$@"
